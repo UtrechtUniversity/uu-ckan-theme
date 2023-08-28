ade3bde178f84c91643bb2843dbde5dbabf04fa4
Neha Moopen <neha.moopen@gmail.com>
2023-08-28 11:50:04 +0200

    URL fix

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/nehamoopen/uu-ckan-theme',
+    url='https://github.com/UtrechtUniversity/uu-ckan-theme',
 
     # Author details
     author='''UU''',
